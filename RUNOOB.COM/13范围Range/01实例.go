0e6eef411684469c73a0c0b80e68dc2440c49990
Goular <zhaojt_cs@126.com>
2017-10-20 19:08:36 +0800

    提交Range范围的内容

---
+++ RUNOOB.COM/13范围Range/01实例.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Printf("Sum:%d", sum)
+	//Go 语言中 range 关键字用于for循环中迭代数组(array)、切片(slice)、通道(channel)或集合(map)的元素。在数组和切片中它返回元素的索引值，在集合中返回 key-value 对的 key 值。
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("Index：", i)
+		}
+	}
+	//range也可以用在Map的键值对上
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+	//range也可以用来枚举Unicode字符串。第一个参数是字符的索引，第二个是字符（Unicode的值）本身。
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
