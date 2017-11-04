48d8c9dd24f90867bd348eb8b7539a8dc3799e5a
Goular <zhaojt_cs@126.com>
2017-11-05 03:17:06 +0800

    内容提交

--- BeiFengTech/第22讲 Beego框架简介/src/WEB/controllers/testInput.go
+++ BeiFengTech/第22讲 Beego框架简介/src/WEB/controllers/testInput.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"API/models"
+	"encoding/json"
+)
 
 type TestInputController struct {
 	beego.Controller
@@ -12,8 +16,26 @@ type User struct {
 }
 
 func (this *TestInputController) Get() {
-	id:=this.GetString("id")//方法一
-	name := this.Input().Get("name")//方法二
+	id := this.GetString("id")       //方法一
+	name := this.Input().Get("name") //方法二
 	this.Ctx.WriteString("<html>" + id + "<br/>")
 	this.Ctx.WriteString(name + "</html>")
+}
+
+//func (this *TestInputController) Post() {
+//	u := new(User)
+//	if err := this.ParseForm(u); err != nil {
+//		this.Ctx.WriteString("异常错误")
+//	}
+//	this.Ctx.WriteString("<html>" + u.Username + "<br/>")
+//	this.Ctx.WriteString(u.Password + "</html>")
+//}
+
+//在配置文件里设置 copyrequestbody = true
+func (this *TestInputController) Post() {
+	var ob models.Object
+	json.Unmarshal(this.Ctx.Input.RequestBody, &ob)
+	objectid := models.AddOne(ob)
+	this.Data["json"] = "{\"ObjectId\":\"" + objectid + "\"}"
+	this.ServeJSON()
 }
