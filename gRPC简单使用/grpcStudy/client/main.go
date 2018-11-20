4cb6c30d15d344d8a58c7421fa349adbbf9b337b
goular <zhaojt_exam@126.com>
2018-11-20 23:44:45 +0800

    提交内容

---
+++ gRPC简单使用/grpcStudy/client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "grpcStudy/helloworld"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatal("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
